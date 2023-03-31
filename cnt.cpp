7a8e06e8de655c950736cc7dd782cd91d19949cc
kykougaku <6489550+kykougaku@users.noreply.github.com>
2023-03-31 23:02:02 +0900

    updated

--- cnt.cpp
+++ cnt.cpp
@@ -11,10 +11,22 @@ using namespace std;
 double Atom::getx() const{return this->coord(0);}
 double Atom::gety() const{return this->coord(1);}
 double Atom::getz() const{return this->coord(2);}
-void getcoord(Vector3d &coord) const {
+void Atom::getcoord(Vector3d &coord) const {
 	coord = this->coord;
 }
 Atom::Atom(Vector3d &coord){
 	this->coord = coord;
 }
 //Bond//////////////////////////////////////////////////
+void Bond::getcoord(Vector3d &coord) const {
+	coord = this->coord;
+}
+void Bond::getangle(Vector3d &angle) const {
+	angle = this->angle;
+}
+Bond::Bond(Vector3d &a0, Vector3d &a1){
+	this->coord = (a0 + a1) / 2;
+	this->angle(0) = atan( (a1(1)-a0(1)) / (a1(2)-a0(2)) );
+	this->angle(1) = atan( (a1(0)-a0(0)) / (a1(2)-a0(2)) );
+	this->angle(2) = 0.0;
+}
