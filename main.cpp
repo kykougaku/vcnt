6109e45410ab282f6fe78a4882ba2c437eb9199d
kykougaku <6489550+kykougaku@users.noreply.github.com>
2023-04-02 16:04:11 +0900

    updated

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include<fstream>
 #include<string>
 #include"cnt.hpp"
+#include"timer_m.hpp"
 
 using namespace Eigen;
 using namespace std;
@@ -12,7 +13,14 @@ int main(){
     int n,m;
     cout<<"input chilarity as n_m"<<endl;
     cin>>n>>m;
+
+    timer::timemeasurer timer;
+    timer.timerstart();
     //NanoTube *test = new NanoTube(n,m);
     NanoTube test(n,m);
+
+    timer.timerend();
+	std::cout << timer.result() << "ms"<<std::endl;
+
     return 0;
 }
